59b3746ee26c85206e0f20be868a5ffcdde389a0
DarioBronders <dariobronders123@hotmail.com>
2022-09-29 12:47:56 +0200

    add sql script

---
+++ src/main/java/util/Insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('26', 'België,Canada', '21');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('27', 'Brazilië,Zwitserland', '18');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('28', 'Marroko,Kroatië', '15');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('28', 'Spanje,Duitsland', '18');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('30', 'Frankrijk,Denemarken', '15');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('30', 'Argentinië,Mexico', '18');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('31', 'Engeland,USA', '18');
+INSERT INTO `fifaworldcup`.`wedstrijd` (`dag`, `landen`, `uur`) VALUES ('31', 'Nederland,Qatar', '21');
+
+INSERT INTO `fifaworldcup`.`stadium` (`name`) VALUES ('Al Thumama Stadium');
+INSERT INTO `fifaworldcup`.`stadium` (`name`) VALUES ('Al Bayt Stadium');
+
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('35', '1', '1');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('21', '1', '2');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('5', '1', '3');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('95', '2', '4');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('45', '2', '5');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('10', '1', '6');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('22', '1', '7');
+INSERT INTO `fifaworldcup`.`wedstrijdticket` (`tickets`, `stadium_id`, `wedstrijd_id`) VALUES ('16', '2', '8');
+
